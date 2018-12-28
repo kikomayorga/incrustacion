abbf23c565b4bcd988eb400792e7e2b6df1a7be8
kikomayorga <kikomayorga@gmail.com>
2018-12-28 14:30:00 -0500

    incrustacion

---
+++ incrustacion.ino
@@ -0,0 +1,69 @@
+#include <LowPower.h>
+
+int gatillo_n = 0;
+boolean cerebro = 0; 
+boolean habil = 0; 
+
+
+
+// entre 44 y 48 debe estar el mínimo. 
+
+void setup() {
+  // AO entrada gatillo
+  // D10 salida PWM
+  // D2 habilitador DOUT de GPS
+  // D6 status energía cerebro
+  
+  pinMode(2, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(10, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  gatillo_n = analogRead(0) / 4;
+
+
+  if(gatillo_n <= 48){
+    gatillo_n = 48;
+  }
+
+
+  
+  if(gatillo_n > 48){
+    gatillo_n = 48 + ((gatillo_n - 48) * 3)/2; 
+  }
+
+  if(gatillo_n > 255){
+    gatillo_n = 255; 
+  }
+
+  cerebro = digitalRead(6);
+  habil = digitalRead(2);
+
+  if(habil == 0){
+    gatillo_n = 46;    
+  }
+  
+  if(cerebro == 0){
+    gatillo_n = 0;
+    delay(50);
+    Serial.println("entrando a sleep mode");
+    delay(100);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);   
+  }
+
+
+    
+  analogWrite(10, gatillo_n);
+  
+  Serial.print("Gatillo n: ");
+  Serial.print(gatillo_n);
+  Serial.print("    Energía en Cerebro:    ");
+  Serial.print(cerebro);
+  Serial.print("    Habilitado por GPS:    ");
+  Serial.println(habil);
+  
+}
